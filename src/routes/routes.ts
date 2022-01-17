64c70f46aae41f1b935b066ace9ac7e90e1f8eeb
Armando Belara <armandobelara@gmail.com>
2022-01-17 17:24:44 +0800

    Adding validation middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 import {Router} from 'https://deno.land/x/oak@v6.4.1/mod.ts';
 import {Register} from '../controllers/auth.controller.ts';
+import { RegisterValidation } from '../validations/register.validations.ts';
 
 const router = new Router();
 router
-  .post('/api/register', Register);
+  .post('/api/register', RegisterValidation, Register);
 
 export default router;
